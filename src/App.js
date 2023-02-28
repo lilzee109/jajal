0dcfb248a7bbc11b0ca9557837277223acff8481
Muhammad Safaat <paatlupi01@gmail.com>
2023-02-28 19:32:38 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onFormSubmit = async (event) => {
+    event.preventDefault()
+    await axios.post(`${process.env.REACT_APP_LINK_API}login`, {
+      email: email,
+      password: password
+    }).then((res) => {
+      console.log(res.data)
+      auth()
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const auth = useCallback(() => {
+    axios.get(`${process.env.REACT_APP_LINK_API}auth`)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  useEffect(() => {
+    auth()
+  }, [auth])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>login</p>
+      <form onSubmit={onFormSubmit}>
+        <input value={email} type="text" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <input value={password} type="text" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={onFormSubmit}>submit</button>
+      </form>
+    </>
   );
 }
 
